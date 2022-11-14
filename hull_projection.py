b3796a9da5f8cf5c70453e521044fa3739b46747
vukadinovic936 <milosvuk99@gmail.com>
2022-11-13 22:59:13 -0800

    hull projection test

--- hull_projection.py
+++ hull_projection.py
@@ -12,6 +12,7 @@ def project_to_3d(aligned_coords, points_hull, Tmatrix):
 	Return:
 		prediction float64 numpy array shape=(n,x,y,z)
 	"""
+	## I hardcode this for now until output from CT alignment is correct
 	aligned_coords = np.array([[203, 83.00006068],
 						[198,  76.00005692],
 						[192,  70.00005567],
@@ -37,3 +38,21 @@ def project_to_3d(aligned_coords, points_hull, Tmatrix):
 #         			 [ 42.88512421, -39.83932495,  47.71887207],
 #        			 [ 39.88512421, -31.83932495,  50.71887207],
 #        			 [ 43.88512421, -21.83932495,  51.71887207]])
+if __name__ == "__main__":
+	aligned_coords = np.array([[203, 83.00006068],
+					[198,  76.00005692],
+					[192,  70.00005567],
+					[185,  64.00005567],   
+					[177, 60.00005692],
+					[169, 57.00005317],
+					[161,  54.00004941],
+					[151, 53.00005442]])
+	import scipy.io
+	import nibabel as nib
+	hull = scipy.io.loadmat("DBS_bT20/hull_rh.mat")
+	points_hull=np.array(hull['mask_indices'])	
+
+	prect = nib.load("DBS_bt20/rpreop_ct.nii")
+	Tmatrix = np.transpose(prect.affine)
+
+	print(project_to_3d(aligned_coords, points_hull, Tmatrix))
