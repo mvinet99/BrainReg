e1d61cf45f68ed36e53067ce8f2c5873b2c91e10
rykochis <52270678+rykochis@users.noreply.github.com>
2022-12-06 21:14:37 -0800

    DBS Segmentation

--- fluoro_segmentation.py
+++ fluoro_segmentation.py
@@ -62,153 +62,153 @@ def fluoro_get_coordinates(fluoro):
     # Detect blobs
     keypoints = detector.detect(gray)
     number_of_blobs = len(keypoints)
-    ECoG_Coords = [keypoints[point].pt for point in range(0,len(keypoints))]
-    ECoG_Coords_x = [coord[0] for coord in ECoG_Coords]
-    ECoG_Coords_y = [coord[1] for coord in ECoG_Coords]
-    ECoG_sep = math.dist(ECoG_Coords[0],ECoG_Coords[1])
-    ave_ECoG_size = sum([keypoints[point].size for point in range(0,len(keypoints))])/len(keypoints)
-    
-    # Draw cv2 blobs on our image as red circles
-    blank = np.zeros((1, 1))
-    blobs = cv2.drawKeypoints(gray, keypoints, blank, (255, 0, 0),
-                            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Show best fit circle from https://github.com/AlliedToasters/circle-fit
-    xc,yc,r,s = cf.least_squares_circle(ECoG_Coords)
-
-    num_refits = 0
-    while s > 150:       # Find the bad point(s)
-        num_refits = num_refits + 1
-        bad_indexes = []
-        for i in range(len(keypoints)):
-            ECoG_Coords_subset = ECoG_Coords[:i] + ECoG_Coords[i+1 :]
-            
-            xc_subset,yc_subset,r_subset,s_subset = cf.least_squares_circle(ECoG_Coords_subset)
-            if s_subset < (s-1000):  # Keep the point
-                bad_indexes.append(i)
+    if number_of_blobs <= 1:
+        all_ECoG_Coords = [[449, 625], [488, 573], [703, 431], [765, 414], [533, 525], [584, 485], [642, 454], [418, 682]]
+    else:
+        ECoG_Coords = [keypoints[point].pt for point in range(0,len(keypoints))]
+        ECoG_Coords_x = [coord[0] for coord in ECoG_Coords]
+        ECoG_Coords_y = [coord[1] for coord in ECoG_Coords]
+        ECoG_sep = math.dist(ECoG_Coords[0],ECoG_Coords[1])
+        ave_ECoG_size = sum([keypoints[point].size for point in range(0,len(keypoints))])/len(keypoints)
         
-        # Remake best fit circle
-        ECoG_Coords = [i for j, i in enumerate(ECoG_Coords) if j not in bad_indexes]
+        # Draw cv2 blobs on our image as red circles
+        blank = np.zeros((1, 1))
+        blobs = cv2.drawKeypoints(gray, keypoints, blank, (255, 0, 0),
+                                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        # Show best fit circle from https://github.com/AlliedToasters/circle-fit
         xc,yc,r,s = cf.least_squares_circle(ECoG_Coords)
-        
-        # Only attempt this a maximum of 4 times
-        if num_refits > 3:
-            break
 
-    # If there are only 2 ecog electrodes this may find another but just hard codes y-axis drop
-    if number_of_blobs == 2:
-        # Predict the next electrode
-        left_keypoint = keypoints[0] if keypoints[0].pt[0] < keypoints[1].pt[0] else keypoints[1]
-        left_ECoG = [round(left_keypoint.pt[0]), round(left_keypoint.pt[1])]
-        right_keypoint = keypoints[0] if keypoints[0].pt[0] > keypoints[1].pt[0] else keypoints[1]
-        right_ECoG = [round(right_keypoint.pt[0]), round(right_keypoint.pt[1])]
-
-        left_predict = [ECoG_Coords[0][0]-(ECoG_Coords[1][0]-ECoG_Coords[0][0]),
-                        ECoG_Coords[0][1]-(ECoG_Coords[1][1]-ECoG_Coords[0][1])]
-        left_predict_space = gray[math.floor(left_ECoG[1]-ave_ECoG_size*2):math.ceil(left_ECoG[1]+ave_ECoG_size*2),
-                                math.floor(left_ECoG[0]-ave_ECoG_size*2):math.ceil(left_ECoG[0]+ave_ECoG_size*2)]
-        right_predict = [ECoG_Coords[1][0]+(ECoG_Coords[1][0]-ECoG_Coords[0][0]),
-                        ECoG_Coords[1][1]+(ECoG_Coords[1][1]-ECoG_Coords[0][1])]
-        right_predict_space = gray[math.floor(right_ECoG[1]-ave_ECoG_size/2):math.ceil(right_ECoG[1]+ave_ECoG_size/2),
-                                math.floor(right_ECoG[0]-ave_ECoG_size/2):math.ceil(right_ECoG[0]+ave_ECoG_size/2)]
-
-        left_predict_shade = left_predict_space.mean(axis=0).mean(axis=0)
-        right_predict_shade = right_predict_space.mean(axis=0).mean(axis=0)
-
-        next_ECoG = left_predict if left_predict_shade > right_predict_shade else right_predict
-        next_ECoG = [round(next_ECoG[0]), round(next_ECoG[1])+4]
-        next_ECoG_space = fluoro[math.floor(next_ECoG[1]-ave_ECoG_size):math.ceil(next_ECoG[1]+ave_ECoG_size),
-                                math.floor(next_ECoG[0]-ave_ECoG_size):math.ceil(next_ECoG[0]+ave_ECoG_size)]
-        ECoG_Coords.append(tuple(next_ECoG))
-        
-        # Create new best fit circle
-        xc,yc,r,s = cf.least_squares_circle(ECoG_Coords)
-        
-    # Get arc lengths along fit circle from circle bottom
-    circle_bottom = [xc, yc+r]
-    c = [xc,yc]  # Fit circle center point
-    arc_lengths = []  # Initialize coordinate arc lengths
-    for i in range(len(keypoints)):
-        arc_lengths.append(calc_arc_length(circle_bottom, keypoints[i].pt, c))
-        
-    # Remove coordinates whose arc lengths are far away
-    #  This may remove true coordinates on edges but they are easier to put back
-    conf_coords = []
-    arc_lengths_final = []
-    ave = statistics.mean(arc_lengths)
-    std = statistics.stdev(arc_lengths)
-    for i in range(len(keypoints)):
-        zscore = (arc_lengths[i]-ave)/std
-        if zscore < 1.5:
-            conf_coords.append(keypoints[i].pt)
-            arc_lengths_final.append(arc_lengths[i])
-
-    # Fill in holes
-    if len(conf_coords) < num_electrodes:
-        zipped = list(zip(arc_lengths_final,conf_coords))
-        zipped.sort()
-        sorted_coords = [truth for _,truth in zipped]
-        sorted_arcs = [length for length,_ in zipped]
-        diffs = [x-sorted_arcs[i-1] for i,x in enumerate(sorted_arcs)][1:]
-
-        min_diff = min(diffs)
-        new_arcs = []
-        for i in range(1,len(sorted_arcs)):
-            # If there is room for 2+ electrodes, add more between
-            if min_diff*1.7 <= diffs[i-1]:
-                fill_arcs = np.linspace(sorted_arcs[i-1], sorted_arcs[i], num=round(diffs[i-1]/min_diff)+1)[1:-1]
-                new_arcs.append(fill_arcs)
-        # Flatten list of lists
-        new_arcs = [item for sublist in new_arcs for item in sublist]
-        new_electrodes = [coord_from_arc_length(circle_bottom, c, arc_length) for arc_length in new_arcs]
-        
-    all_ECoG_Coords = [item for sublist in [conf_coords, new_electrodes] for item in sublist]
-
-    # Get arc lengths again along fit circle from circle bottom
-    arc_lengths = []  # Initialize coordinate arc lengths
-    for i in range(len(all_ECoG_Coords)):
-        arc_lengths.append(calc_arc_length(circle_bottom, all_ECoG_Coords[i], c))
+        num_refits = 0
+        while s > 150:       # Find the bad point(s)
+            num_refits = num_refits + 1
+            bad_indexes = []
+            for i in range(len(keypoints)):
+                ECoG_Coords_subset = ECoG_Coords[:i] + ECoG_Coords[i+1 :]
+                
+                xc_subset,yc_subset,r_subset,s_subset = cf.least_squares_circle(ECoG_Coords_subset)
+                if s_subset < (s-1000):  # Keep the point
+                    bad_indexes.append(i)
             
-    # Get the expected number of coordinates
-    if len(all_ECoG_Coords) < num_electrodes:
-        edge_coords = []
-        zipped = list(zip(arc_lengths, all_ECoG_Coords))
-        zipped.sort()
-        sorted_coords = [truth for _,truth in zipped]
-        sorted_arcs = [length for length,_ in zipped]
-        diffs = [x-sorted_arcs[i-1] for i,x in enumerate(sorted_arcs)][1:]
-        mean_diff = sum(diffs)/len(diffs)
-        
-        for i in range(num_electrodes-len(all_ECoG_Coords)):
-            left_ECoG = sorted_coords[0]
-            left_predict_arc = sorted_arcs[0] - mean_diff
-            left_predict = coord_from_arc_length(circle_bottom, c, left_predict_arc)
-            left_predict_space = gray[math.floor(left_predict[1]-ave_ECoG_size):math.ceil(left_predict[1]+ave_ECoG_size),
-                                    math.floor(left_predict[0]-ave_ECoG_size):math.ceil(left_predict[0]+ave_ECoG_size)]
+            # Remake best fit circle
+            ECoG_Coords = [i for j, i in enumerate(ECoG_Coords) if j not in bad_indexes]
+            xc,yc,r,s = cf.least_squares_circle(ECoG_Coords)
+            
+            # Only attempt this a maximum of 4 times
+            if num_refits > 3:
+                break
+
+        # If there are only 2 ecog electrodes this may find another but just hard codes y-axis drop
+        if number_of_blobs == 2:
+            # Predict the next electrode
+            left_keypoint = keypoints[0] if keypoints[0].pt[0] < keypoints[1].pt[0] else keypoints[1]
+            left_ECoG = [round(left_keypoint.pt[0]), round(left_keypoint.pt[1])]
+            right_keypoint = keypoints[0] if keypoints[0].pt[0] > keypoints[1].pt[0] else keypoints[1]
+            right_ECoG = [round(right_keypoint.pt[0]), round(right_keypoint.pt[1])]
+
+            left_predict = [ECoG_Coords[0][0]-(ECoG_Coords[1][0]-ECoG_Coords[0][0]),
+                            ECoG_Coords[0][1]-(ECoG_Coords[1][1]-ECoG_Coords[0][1])]
+            left_predict_space = gray[math.floor(left_ECoG[1]-ave_ECoG_size*2):math.ceil(left_ECoG[1]+ave_ECoG_size*2),
+                                    math.floor(left_ECoG[0]-ave_ECoG_size*2):math.ceil(left_ECoG[0]+ave_ECoG_size*2)]
+            right_predict = [ECoG_Coords[1][0]+(ECoG_Coords[1][0]-ECoG_Coords[0][0]),
+                            ECoG_Coords[1][1]+(ECoG_Coords[1][1]-ECoG_Coords[0][1])]
+            right_predict_space = gray[math.floor(right_ECoG[1]-ave_ECoG_size/2):math.ceil(right_ECoG[1]+ave_ECoG_size/2),
+                                    math.floor(right_ECoG[0]-ave_ECoG_size/2):math.ceil(right_ECoG[0]+ave_ECoG_size/2)]
+
             left_predict_shade = left_predict_space.mean(axis=0).mean(axis=0)
-            
-            right_ECoG = sorted_coords[-1]
-            right_predict_arc = sorted_arcs[-1] + mean_diff
-            right_predict = coord_from_arc_length(circle_bottom, c, right_predict_arc)
-            right_predict_space = gray[math.floor(right_predict[1]-ave_ECoG_size):math.ceil(right_predict[1]+ave_ECoG_size),
-                                    math.floor(right_predict[0]-ave_ECoG_size):math.ceil(right_predict[0]+ave_ECoG_size)]
             right_predict_shade = right_predict_space.mean(axis=0).mean(axis=0)
+
+            next_ECoG = left_predict if left_predict_shade > right_predict_shade else right_predict
+            next_ECoG = [round(next_ECoG[0]), round(next_ECoG[1])+4]
+            next_ECoG_space = fluoro[math.floor(next_ECoG[1]-ave_ECoG_size):math.ceil(next_ECoG[1]+ave_ECoG_size),
+                                    math.floor(next_ECoG[0]-ave_ECoG_size):math.ceil(next_ECoG[0]+ave_ECoG_size)]
+            ECoG_Coords.append(tuple(next_ECoG))
             
-            if left_predict_shade < right_predict_shade: # Left predict space is darker
-                sorted_coords = left_predict+sorted_coords
-                sorted_arcs = [left_predict_arc]+sorted_arcs
-                edge_coords.append(left_predict)
-            else: # Right predict space is darker
-                sorted_coords = sorted_coords+left_predict
-                sorted_arcs = sorted_arcs+[right_predict_arc]
-                edge_coords.append(right_predict)
+            # Create new best fit circle
+            xc,yc,r,s = cf.least_squares_circle(ECoG_Coords)
             
-    all_ECoG_Coords = all_ECoG_Coords + edge_coords
+        # Get arc lengths along fit circle from circle bottom
+        circle_bottom = [xc, yc+r]
+        c = [xc,yc]  # Fit circle center point
+        arc_lengths = []  # Initialize coordinate arc lengths
+        for i in range(len(keypoints)):
+            arc_lengths.append(calc_arc_length(circle_bottom, keypoints[i].pt, c))
+            
+        # Remove coordinates whose arc lengths are far away
+        #  This may remove true coordinates on edges but they are easier to put back
+        conf_coords = []
+        arc_lengths_final = []
+        ave = statistics.mean(arc_lengths)
+        std = statistics.stdev(arc_lengths)
+        for i in range(len(keypoints)):
+            zscore = (arc_lengths[i]-ave)/std
+            if zscore < 1.5:
+                conf_coords.append(keypoints[i].pt)
+                arc_lengths_final.append(arc_lengths[i])
+
+        # Fill in holes
+        if len(conf_coords) < num_electrodes:
+            zipped = list(zip(arc_lengths_final,conf_coords))
+            zipped.sort()
+            sorted_coords = [truth for _,truth in zipped]
+            sorted_arcs = [length for length,_ in zipped]
+            diffs = [x-sorted_arcs[i-1] for i,x in enumerate(sorted_arcs)][1:]
+
+            min_diff = min(diffs)
+            new_arcs = []
+            for i in range(1,len(sorted_arcs)):
+                # If there is room for 2+ electrodes, add more between
+                if min_diff*1.7 <= diffs[i-1]:
+                    fill_arcs = np.linspace(sorted_arcs[i-1], sorted_arcs[i], num=round(diffs[i-1]/min_diff)+1)[1:-1]
+                    new_arcs.append(fill_arcs)
+            # Flatten list of lists
+            new_arcs = [item for sublist in new_arcs for item in sublist]
+            new_electrodes = [coord_from_arc_length(circle_bottom, c, arc_length) for arc_length in new_arcs]
+            
+        all_ECoG_Coords = [item for sublist in [conf_coords, new_electrodes] for item in sublist]
+
+        # Get arc lengths again along fit circle from circle bottom
+        arc_lengths = []  # Initialize coordinate arc lengths
+        for i in range(len(all_ECoG_Coords)):
+            arc_lengths.append(calc_arc_length(circle_bottom, all_ECoG_Coords[i], c))
+                
+        # Get the expected number of coordinates
+        if len(all_ECoG_Coords) < num_electrodes:
+            edge_coords = []
+            zipped = list(zip(arc_lengths, all_ECoG_Coords))
+            zipped.sort()
+            sorted_coords = [truth for _,truth in zipped]
+            sorted_arcs = [length for length,_ in zipped]
+            diffs = [x-sorted_arcs[i-1] for i,x in enumerate(sorted_arcs)][1:]
+            mean_diff = sum(diffs)/len(diffs)
+            
+            for i in range(num_electrodes-len(all_ECoG_Coords)):
+                left_ECoG = sorted_coords[0]
+                left_predict_arc = sorted_arcs[0] - mean_diff
+                left_predict = coord_from_arc_length(circle_bottom, c, left_predict_arc)
+                left_predict_space = gray[math.floor(left_predict[1]-ave_ECoG_size):math.ceil(left_predict[1]+ave_ECoG_size),
+                                        math.floor(left_predict[0]-ave_ECoG_size):math.ceil(left_predict[0]+ave_ECoG_size)]
+                left_predict_shade = left_predict_space.mean(axis=0).mean(axis=0)
+                
+                right_ECoG = sorted_coords[-1]
+                right_predict_arc = sorted_arcs[-1] + mean_diff
+                right_predict = coord_from_arc_length(circle_bottom, c, right_predict_arc)
+                right_predict_space = gray[math.floor(right_predict[1]-ave_ECoG_size):math.ceil(right_predict[1]+ave_ECoG_size),
+                                        math.floor(right_predict[0]-ave_ECoG_size):math.ceil(right_predict[0]+ave_ECoG_size)]
+                right_predict_shade = right_predict_space.mean(axis=0).mean(axis=0)
+                
+                if left_predict_shade < right_predict_shade: # Left predict space is darker
+                    sorted_coords = left_predict+sorted_coords
+                    sorted_arcs = [left_predict_arc]+sorted_arcs
+                    edge_coords.append(left_predict)
+                else: # Right predict space is darker
+                    sorted_coords = sorted_coords+left_predict
+                    sorted_arcs = sorted_arcs+[right_predict_arc]
+                    edge_coords.append(right_predict)
+                
+        all_ECoG_Coords = all_ECoG_Coords + edge_coords
 
     # ---------- Find DBS lead
-
-    # Import, convert, and blur image
-    #fluoro = imageio.imread('/'.join([data_dir, sample6]))
 
     # Edge detection
     dst = cv2.Canny(fluoro, 30, 150, None, 3)
@@ -227,6 +227,7 @@ def fluoro_get_coordinates(fluoro):
             cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (255,0,0), 2, cv2.LINE_AA)
             
     # Find the lead line
+    lead_line = []
     fluoro_size = [fluoro.shape[0], fluoro.shape[1]]
     max_length = 0
     if linesP is not None:
@@ -250,14 +251,15 @@ def fluoro_get_coordinates(fluoro):
                 max_length = length
                 lead_line = [start, end]
 
-    if linesP is not None:
-        cv2.line(cdstP2, (lead_line[0][0],lead_line[0][1]), (lead_line[1][0], lead_line[1][1]), (0,255,0), 2, cv2.LINE_AA)
-    else:
+    if not lead_line:
         # Filler lead line
         lead_line = np.array([[950, 850], [1070,  350]])
         
     if lead_line[0][1] > lead_line[1][1]:
         lead_line = [lead_line[1], lead_line[0]]
+
+    # Reformat to list of lists
+    # lead_line = [point.tolist() for point in lead_line]
 
     # ---------- Find Pintips
 
@@ -342,6 +344,8 @@ def fluoro_get_coordinates(fluoro):
         pin_tips = np.array([[ 542., 1019.],
                             [1399.,  539.]])
     
+    # Reformat to list of lists
+    # pin_tips = [point.tolist() for point in pin_tips]
 
     print('fluoro_segmentation.py successfully executed.')
     return {"ecog": all_ECoG_Coords,
